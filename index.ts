0d709770ccb4fe003935b8b3350d3214692fc14e
hideto.fukuda <hifukukda5822@gmail.com>
2023-11-05 22:47:38 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,44 +1,40 @@
-
 // https://github.com/Dellos7/mosaic-node-generator/tree/master
-var mosaic = require('mosaic-node-generator');
-import * as ffmpeg from 'fluent-ffmpeg';
+var mosaic = require("mosaic-node-generator");
+// import * as mosaic from "mosaic-node-generator";
+import * as ffmpeg from "fluent-ffmpeg";
 // import { exec, execSync } from 'child_process';
 
-const materialGazoPath = "";
-const baseGazoPath = "aaa"
+const materialGazoPath = "imput.mp4";
+const baseGazoPath = "aaa";
 const materialMoviePath = "";
-const outputGazoPath = "aaa"
+const outputGazoPath = "outputGazoPath";
 
+class HelloWorld {
+  constructor(public displayText: string) {}
 
-class HelloWorld
-{
-    constructor(public displayText : string) {}
+  greet() {
+    const materialGazoPath = "";
+    const baseGazoPath = "aaa";
 
-    greet()
-    {
-        const materialGazoPath = "";
-        const baseGazoPath = "aaa"
-
-        mosaic.mosaic(baseGazoPath, materialGazoPath)
-        return this.displayText;
-    }
+    mosaic.mosaic(baseGazoPath, materialGazoPath);
+    return this.displayText;
+  }
 }
 
-class SplitMovieToImage{
-    constructor(public input1 : string, input2 : string, input3 : string ) {}
+class SplitMovieToImage {
+  constructor(public input1: string, input2: string, input3: string) {}
 
-    split(){
-        ffmpeg(materialMoviePath)
-        .outputOptions(['-vf', 'fps=5'])  // 1秒ごとにフレームを抽出
-        .on('end', () => {
-          console.log('画像の抽出が完了しました。');
-        })
-        .on('error', (err) => {
-          console.error('エラーが発生しました:', err);
-        })
-        .save(outputGazoPath);
-
-    }
+  split() {
+    ffmpeg(materialMoviePath)
+      .outputOptions(["-vf", "fps=5"]) // 1秒ごとにフレームを抽出
+      .on("end", () => {
+        console.log("画像の抽出が完了しました。");
+      })
+      .on("error", (err: string) => {
+        console.error("エラーが発生しました:", err);
+      })
+      .save("output.jpeg");
+  }
 }
 
-var helloWorld = new HelloWorld("helloworld")
+var helloWorld = new HelloWorld("helloworld");
